54085b33f63388bb9ee665bb817edf337f8b4ec2
David Poulter <djspoulter@gmail.com>
2017-01-21 14:09:26 +0000

    Initial commit

---
+++ server/setup.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='bullet',
+    version='0.0.0',
+    packages=find_packages(exclude=['tests']),
+    url='',
+    license='',
+    author='David Poulter',
+    author_email='djspoulter@gmail.com',
+    description='A bullet journal.',
+    requires=[
+        'Django == 1.10.5'
+    ]
+)
